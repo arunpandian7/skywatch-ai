278d63f4b52caa0293ee549ff545d4a8639ed495
Arun Pandian R <arunrk7.leo@gmail.com>
2020-11-25 23:28:04 +0530

    🚀Added Database Scrapper

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,58 @@
-from skywatchai import FaceNet, MTCNN
+"""
+    Dirver Python File for Skywatch AI - an Face Recogniton based Attendance Monitor
+"""
 
-model = FaceNet.loadModel()
-detector = MTCNN.loadModel()
+__author__ = "Arun Pandian R"
+__version__ = "0.0.1"
+__license__ = "MIT"
+
+import argparse
+import os
+import glob
+
+def get_names(dir):
+    try:
+        names = os.listdir(dir)
+    except FileNotFoundError:
+        raise FileNotFoundError("Couldn't find the image in directories. \
+                Please check with the instruction on how to model the folder")
+    if '.gitkeep' in names:
+        names.remove('.gitkeep')
+        return names
+    return names
+
+def get_person_images(dir, names):
+    image_data = {}
+    for name in names:
+        person_img_path = os.path.join(dir, name)
+        image_data[name] = glob.glob(person_img_path+'/*.jpg')
+        image_data[name].extend(glob.glob(person_img_path+'/*.png'))
+    return image_data
+
+def main(args):
+    """ Main entry point of the app """
+    if args.mode == 'transform' and args.directory != None:
+        name_list = get_names(args.directory)
+        print(get_person_images(args.directory, name_list))
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+
+    # Required argument to execute the function
+    parser.add_argument("mode", help="Pass the keyword to execute")
+
+    # Optional argument which requires a parameter (eg. -d test)
+    parser.add_argument("-n", "--dir", action="store", dest="directory")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="SkywatchAI - version {version}".format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)
+
 
