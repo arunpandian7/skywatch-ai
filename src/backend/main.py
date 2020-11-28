891ac8913a64441c41f5af555ccc8ec50fc5d0cc
Arun Pandian R <arunrk7.leo@gmail.com>
2020-11-28 23:46:28 +0530

    Reduced Slugs

--- src/backend/main.py
+++ src/backend/main.py
@@ -7,46 +7,13 @@ __version__ = "0.0.1"
 __license__ = "MIT"
 
 import argparse
-import os
-import glob
 
-from numpy.lib.type_check import imag
-from skywatchai.SkywatchAI import get_face_embedding
 
-def get_names(dir):
-    try:
-        names = os.listdir(dir)
-    except FileNotFoundError:
-        raise FileNotFoundError("Couldn't find the image in directories. \
-                Please check with the instruction on how to model the folder")
-    if '.gitkeep' in names:
-        names.remove('.gitkeep')
-        return names
-    return names
-
-def get_person_images(dir, names):
-    image_data = {}
-    for name in names:
-        person_img_path = os.path.join(dir, name)
-        image_data[name] = glob.glob(person_img_path+'/*.jpg')
-        image_data[name].extend(glob.glob(person_img_path+'/*.png'))
-    return image_data
-
-def create_embedding_data(dir, names):
-    image_data = get_person_images(dir, names)
-    embedding_data = {}
-    for person in image_data.keys():
-        embedding_data[person] = list()
-        for image in image_data[person]:
-            embedding = get_face_embedding(image)
-            embedding_data[person].append(embedding)
-    return embedding_data
 
 def main(args):
     """ Main entry point of the app """
     if args.mode == 'transform' and args.directory != None:
-        name_list = get_names(args.directory)
-        print(create_embedding_data(args.directory, name_list))
+        
 
 
 if __name__ == "__main__":
