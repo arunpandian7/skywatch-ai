bbb846982d1eeb4e91df8fb2700b982577489d80
Arun Pandian R <arunrk7.leo@gmail.com>
2020-11-28 18:38:03 +0530

    :sparkles: ANN Tree Database

--- src/backend/skywatchai/skywatchai.py
+++ src/backend/skywatchai/skywatchai.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import pickle
+from annoy import AnnoyIndex
 from .model.FaceNet import loadmodel
 from .preprocessing import get_face_embedding
 from .utils import getEuclideanDistance
@@ -15,7 +17,8 @@ def get_people_names(dir):
         return names
     return names
 
-def get_image_paths(dir, names):
+def get_image_paths(dir):
+    names = get_people_names(dir)
     image_data = {}
     for name in names:
         person_img_path = os.path.join(dir, name)
@@ -29,7 +32,9 @@ class FaceID():
         self.threshold = 0.80
         self.enforce_detection = enforce_detection
         self.dir = dir
+        self.emb_save_path = os.path.join(dir, 'face_embeds.ann')
         input_shape = self.emb_model.layers[0].input_shape
+        self.embedding_size = 128
         try:
             self.image_size = input_shape[0][1:3]
         except:
@@ -41,4 +46,30 @@ class FaceID():
         dist = getEuclideanDistance(emb1, emb2)
         return True if dist < self.threshold else False
     
+    def build_face_db(self):
+        face_tree = AnnoyIndex(self.embedding_size, 'euclidean')
+        image_paths = get_image_paths(self.dir)
+        i = 1
+        person_id_map = {}
+        for person, images in image_paths:
+            for image in images:
+                embedding = get_face_embedding(image, self.emb_model, self.image_size)
+                face_tree.add_item(i, embedding)
+                person_id_map[i] = person
+                i += 1
+        face_tree.build(5)
+        try:
+            face_tree.save(self.emb_save_path)
+            save_file = open('../../database/personMap.pickle', 'wb')
+            pickle.dump(person_id_map, save_file)
+            save_file.close()
+        except:
+            SystemError('Cannot save data files')
+    
+    
+
+
+            
+
+
 
