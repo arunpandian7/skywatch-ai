2518dc6e1d4e284fb50764e787f206c3a4b6fba2
Arun Pandian R <arunrk7.leo@gmail.com>
2020-12-02 18:18:46 +0530

    Fixed Bugs

--- src/backend/skywatchai/preprocessing.py
+++ src/backend/skywatchai/preprocessing.py
@@ -10,39 +10,41 @@ import cv2
 
 def align_face(img, detector):
     detection = detector.detect_faces(img)
-    keypoints = detection['keypoints']
-    left_eye = keypoints['left_eye']
-    right_eye = keypoints['right_eye']
-    # Alignment is done by rotation with respect to Left and Right Eye
-    left_eye_x, left_eye_y = left_eye
-    right_eye_x, right_eye_y = right_eye
+    if len(detection)>0:
+        detection = detection[0]
+        keypoints = detection['keypoints']
+        left_eye = keypoints['left_eye']
+        right_eye = keypoints['right_eye']
+        # Alignment is done by rotation with respect to Left and Right Eye
+        left_eye_x, left_eye_y = left_eye
+        right_eye_x, right_eye_y = right_eye
 
-    # Finding the direction of rotation
-    if left_eye_y > right_eye_y:
-        point_3rd = (right_eye_x, left_eye_y)
-        direction = -1 #rotate clockwise   
-    else:
-        point_3rd = (left_eye_x, right_eye_y)
-        direction = 1 #rotate counter-clockwise
+        # Finding the direction of rotation
+        if left_eye_y > right_eye_y:
+            point_3rd = (right_eye_x, left_eye_y)
+            direction = -1 #rotate clockwise   
+        else:
+            point_3rd = (left_eye_x, right_eye_y)
+            direction = 1 #rotate counter-clockwise
 
-    # Find lenght of Right Angled Triangle
-    a = getEuclideanDistance(np.array(left_eye), np.array(point_3rd))
-    b = getEuclideanDistance(np.array(right_eye), np.array(point_3rd))
-    c = getEuclideanDistance(np.array(right_eye), np.array(left_eye))
+        # Find lenght of Right Angled Triangle
+        a = getEuclideanDistance(np.array(left_eye), np.array(point_3rd))
+        b = getEuclideanDistance(np.array(right_eye), np.array(point_3rd))
+        c = getEuclideanDistance(np.array(right_eye), np.array(left_eye))
 
-    #-----------------------
-	# Applying Cosine Law
-    if b != 0 and c != 0: #this multiplication causes division by zero in cos_a calculation
-        
-        cos_a = (b*b + c*c - a*a)/(2*b*c)
-        angle = np.arccos(cos_a) #angle in radian
-        angle = (angle * 180) / math.pi #radian to degree
-        if direction == -1:
-            angle = 90 - angle
-        # Rotating Given Image
-        img = Image.fromarray(img)
-        img = np.array(img.rotate(direction * angle))
-    return img
+        #-----------------------
+        # Applying Cosine Law
+        if b != 0 and c != 0: #this multiplication causes division by zero in cos_a calculation
+            
+            cos_a = (b*b + c*c - a*a)/(2*b*c)
+            angle = np.arccos(cos_a) #angle in radian
+            angle = (angle * 180) / math.pi #radian to degree
+            if direction == -1:
+                angle = 90 - angle
+            # Rotating Given Image
+            img = Image.fromarray(img)
+            img = np.array(img.rotate(direction * angle))
+        return img
 
 def get_faces(img, detector, enforce=True):
     if not type(img) == np.ndarray:
